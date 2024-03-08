bb40e7f158cec2d3b8ba9bacb09348b1dcc1664f
hemifunk <hemi.funk@gmail.com>
2024-03-08 10:24:09 +0200

    added tests

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 import unittest
-from program import show_topic_options
-from consts import TOPIC_OPTIONS_KEYBOARD
+from unittest.mock import Mock, patch
+from telegram.ext import ConversationHandler
+
 from unittest.mock import AsyncMock, MagicMock
 from utils import create_keyboard, group_buttons
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-class TestKeyboardUtils(unittest.TestCase):
+from consts import TOPIC_OPTIONS_KEYBOARD
+from program import lessons_videos, communities, study_program, show_topic_options
 
+class TestKeyboardUtils(unittest.TestCase):
     def test_create_keyboard(self):
         buttons = ['button1', 'button2', 'button3']
         result = create_keyboard(buttons)
@@ -43,6 +46,34 @@ class TestShowTopicOptions(unittest.TestCase):
             reply_markup=InlineKeyboardMarkup(TOPIC_OPTIONS_KEYBOARD),
             parse_mode='Markdown'
         )
+
+class TestBotFunctions(unittest.TestCase):
+    @patch('telegram.Update')
+    @patch('telegram.ext.CallbackContext')
+    async def test_lessons_videos(self, mock_update, mock_context):
+        mock_context.user_data = {'topic': 'בעיות מילוליות'}
+        mock_update.callback_query.message.reply_text = Mock()
+        result = await lessons_videos(mock_update, mock_context)
+        self.assertEqual(result, ConversationHandler.END)
+        mock_update.callback_query.message.reply_text.assert_called_once()
+
+    @patch('telegram.Update')
+    @patch('telegram.ext.CallbackContext')
+    async def test_communities(self, mock_update, mock_context):
+        mock_context.user_data = {'topic': 'סדרות'}
+        mock_update.callback_query.message.reply_text = Mock()
+        result = await communities(mock_update, mock_context)
+        self.assertEqual(result, ConversationHandler.END)
+        mock_update.callback_query.message.reply_text.assert_called_once()
+
+    @patch('telegram.Update')
+    @patch('telegram.ext.CallbackContext')
+    async def test_study_program(self, mock_update, mock_context):
+        mock_context.user_data = {'topic': 'פונקציית שורש'}
+        mock_update.callback_query.message.reply_text = Mock()
+        result = await study_program(mock_update, mock_context)
+        self.assertEqual(result, ConversationHandler.END)
+        mock_update.callback_query.message.reply_text.assert_called_once()
 
 
 if __name__ == '__main__':
